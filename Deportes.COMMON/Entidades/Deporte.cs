dc8a951d0af4ca0ef7630a32f136ac870446c337
Cindy <cinnndy420@outlook.es>
2018-04-21 12:30:46 -0500

    Actulizar

---
+++ Deportes.COMMON/Entidades/Deporte.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deportes.COMMON.Entidades
+{
+    public class Deporte:Base
+    {
+        public string NombreDeporte { get; set; }
+
+    }
+}
